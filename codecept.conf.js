b87b67187bafffb7836b72613539e5f79adb0f59
FirmanAB <87408499+FirmanAB@users.noreply.github.com>
2021-07-19 10:53:46 +0700

    Add files via upload
    format only

--- codecept.conf.js
+++ codecept.conf.js
@@ -9,23 +9,23 @@ exports.config = {
   output: 'e2e/outputs',
   helpers: {
     Puppeteer: {
-      url: 'http://localhost:8083',
+      url: 'http://localhost:8080',
       show: true,
-      windowSize: '1200x900'
-    }
+      windowSize: '1200x900',
+    },
   },
   include: {
-    I: './steps_file.js'
+    I: './steps_file.js',
   },
   bootstrap: null,
   mocha: {},
   name: 'restaurant-apps',
   plugins: {
     retryFailedStep: {
-      enabled: true
+      enabled: true,
     },
     screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
+      enabled: true,
+    },
+  },
+};
